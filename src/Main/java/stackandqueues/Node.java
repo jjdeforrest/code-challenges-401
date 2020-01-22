fb4591a8604379e3a086effe58fef2867c5688c3
jjdeforrest <jjdeforrest@users.noreply.github.com>
2020-01-21 21:21:43 -0800

    update

--- src/Main/java/stackandqueues/Node.java
+++ src/Main/java/stackandqueues/Node.java
@@ -1,26 +1,26 @@
-package stackandqueues;
-
-public class Node {
-    int value;
-    Node next;
-
-    public Node(int value) {
-        this(value, null);
-    }
-
-    public Node(int value, Node next) {
-        this.value = value;
-        this.next = next;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    @Override
-    public String toString() {
-        return "Node has a value of" + this.value;
-
-    }
-}
+//package stackandqueues;
+//
+//public class Node {
+//    int value;
+//    Node next;
+//
+//    public Node(int value) {
+//        this(value, null);
+//    }
+//
+//    public Node(int value, Node next) {
+//        this.value = value;
+//        this.next = next;
+//    }
+//
+//    public Node getNext() {
+//        return next;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Node has a value of" + this.value;
+//
+//    }
+//}
 
